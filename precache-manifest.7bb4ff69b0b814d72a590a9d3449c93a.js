fc2154565326d74111fbfd74550c9890612a96ee
anupongiv4 <anuponglive@gmail.com>
2020-07-26 17:03:39 +0700

    Updates

---
+++ precache-manifest.7bb4ff69b0b814d72a590a9d3449c93a.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a6d984597abec65c17a94662381719b",
+    "url": "/amHotel/index.html"
+  },
+  {
+    "revision": "129e78a4cef17dffb9d5",
+    "url": "/amHotel/static/css/main.2add3149.chunk.css"
+  },
+  {
+    "revision": "780fa362c5022c128422",
+    "url": "/amHotel/static/js/2.70df6ee1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/amHotel/static/js/2.70df6ee1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "129e78a4cef17dffb9d5",
+    "url": "/amHotel/static/js/main.b914e4fa.chunk.js"
+  },
+  {
+    "revision": "8fdafc8ac2fb3d467ea6",
+    "url": "/amHotel/static/js/runtime-main.c836a650.js"
+  },
+  {
+    "revision": "d1627228698f8c4fd52369e4e57fd5fb",
+    "url": "/amHotel/static/media/d-double-bed-1.d1627228.jpg"
+  },
+  {
+    "revision": "328cca1e70213bae3f2f0cd9b88db906",
+    "url": "/amHotel/static/media/d-double-bed-2.328cca1e.jpg"
+  },
+  {
+    "revision": "d19b3d2493c17376acea8f5b46e254d8",
+    "url": "/amHotel/static/media/d-double-bed-3.d19b3d24.jpg"
+  },
+  {
+    "revision": "f9400c4e5b1a61a3a445e86bd3177a85",
+    "url": "/amHotel/static/media/d-double-bed-4.f9400c4e.jpg"
+  },
+  {
+    "revision": "2e81d3281437367da056347ced0cb962",
+    "url": "/amHotel/static/media/d-twin-bed-1.2e81d328.jpg"
+  },
+  {
+    "revision": "94f305bcd2474bbc1774c7a35b9637d5",
+    "url": "/amHotel/static/media/d-twin-bed-2.94f305bc.jpg"
+  },
+  {
+    "revision": "5e8b30efd3ea8e50a04ab6e6e0dd33fe",
+    "url": "/amHotel/static/media/d-twin-bed-3.5e8b30ef.jpg"
+  },
+  {
+    "revision": "cb331afa877ee4c28cba8cc69c5e2b52",
+    "url": "/amHotel/static/media/mainbanner.cb331afa.jpg"
+  },
+  {
+    "revision": "792be600a92f58129f56fd4075d5487b",
+    "url": "/amHotel/static/media/p-double-bed-1.792be600.jpg"
+  },
+  {
+    "revision": "d8e0675749e63aa3e5d831fb0ab8f316",
+    "url": "/amHotel/static/media/p-double-bed-2.d8e06757.jpg"
+  },
+  {
+    "revision": "220244f32f58c05f855e226f9cdde16d",
+    "url": "/amHotel/static/media/p-double-bed-3.220244f3.jpg"
+  },
+  {
+    "revision": "a0a1dca8bd319cc86f29bbc30faa7e8d",
+    "url": "/amHotel/static/media/p-twin-bed-1.a0a1dca8.jpg"
+  },
+  {
+    "revision": "e2f00fa4a0adef5e80a1c100605f80e4",
+    "url": "/amHotel/static/media/p-twin-bed-2.e2f00fa4.jpg"
+  },
+  {
+    "revision": "4e0ba604829bba305f5270de130eec1e",
+    "url": "/amHotel/static/media/p-twin-bed-3.4e0ba604.jpg"
+  },
+  {
+    "revision": "6613eb87d0bf4ad2d8b5baed9f6ff12b",
+    "url": "/amHotel/static/media/p-twin-bed-4.6613eb87.jpg"
+  },
+  {
+    "revision": "ffe981528350f8d577fb2dd4845b9793",
+    "url": "/amHotel/static/media/s-double-bed-1.ffe98152.jpg"
+  },
+  {
+    "revision": "1cadac009126635e0575f1f6d561a606",
+    "url": "/amHotel/static/media/s-double-bed-2.1cadac00.jpg"
+  },
+  {
+    "revision": "be74857b8b74c78bcf2de70a0f340a66",
+    "url": "/amHotel/static/media/s-double-bed-3.be74857b.jpg"
+  },
+  {
+    "revision": "b892f3d1b6af546275604e88873f3d44",
+    "url": "/amHotel/static/media/s-double-bed-4.b892f3d1.jpg"
+  },
+  {
+    "revision": "ed8f0e27dd407c5606251b9c31b25c44",
+    "url": "/amHotel/static/media/s-twin-bed-1.ed8f0e27.jpg"
+  },
+  {
+    "revision": "5f3a901cad14d9c90d958f924440e143",
+    "url": "/amHotel/static/media/s-twin-bed-2.5f3a901c.jpg"
+  },
+  {
+    "revision": "c6fffa072d96cd302e7a2f41c2ec52c4",
+    "url": "/amHotel/static/media/s-twin-bed-3.c6fffa07.jpg"
+  },
+  {
+    "revision": "cb214119948f156a0201787e37adfe93",
+    "url": "/amHotel/static/media/s-twin-bed-4.cb214119.jpg"
+  }
+]);
